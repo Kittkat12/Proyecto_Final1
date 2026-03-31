2fd8744f5f0f55acbdb71171d156d6bfcb174ebe
Katherine Cuculista <1311205@galileo.edu>
2026-03-30 21:50:20 -0600

    Deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,21 @@ const connectDB = require("./config/db");
 const habitsRouter = require("./routes/habits");
 const cors = require("cors");
 const userRoutes = require("./routes/users");
-
+const cookieParser = require("cookie-parser");
 dotenv.config();
 connectDB();
-
+const isProduction = process.env.NODE_ENV === "production";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: isProduction ? "URL_DEL_FRONTEND_EN_RENDER" : "http://localhost:3001",
+    credentials: true,
+  })
+);
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/habits", habitsRouter);
 
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-
+const isProduction = process.env.NODE_ENV === "production";
 const router = express.Router();
 
 // Registro
